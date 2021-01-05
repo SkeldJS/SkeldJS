b4efcf064bf7af8126de829529680daf92e446c7
Edward Smale <essmale2005@gmail.com>
2021-01-05 12:16:34 +0000

    Updated sleep test

--- packages/util/ts/lib/sleep.spec.ts
+++ packages/util/ts/lib/sleep.spec.ts
@@ -8,7 +8,7 @@ describe("Sleep", () => {
     it("Should sleep for a specified number of miliseconds in an asynchronous function.", async () => {
         const before = Date.now();
 
-        await sleep(50);
+        await sleep(100);
 
         assert(Date.now() > before + 50);
     });
