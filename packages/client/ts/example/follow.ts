2c6b96553c8d52c10d3089f94f8dfe364e91d754
Edward Smale <essmale2005@gmail.com>
2021-04-08 12:20:23 +0100

    Fixed ventid event, fixed snapto & movement sequence IDs (maybe).

--- packages/client/ts/example/follow.ts
+++ packages/client/ts/example/follow.ts
@@ -1,32 +1,19 @@
-import { ColorID, MessageTag, RpcTag } from "@skeldjs/constant";
 import * as skeldjs from "..";
 
 (async () => {
     const client = new skeldjs.SkeldjsClient("2021.3.25.0");
 
     console.log("Connecting..");
-    await client.connect("EU", "weakeyes", parseInt(process.argv[2]));
-
-    client.on("player.spawn", ev => {
-        if (ev.data.player !== client.me)
-            return;
-
-        console.log("Spawned.");
-        client.me.control.checkColor(ColorID.Blue);
-        client.me.control.checkName("poop");
-
-        setTimeout(() => {
-            client.broadcast([
-                {
-                    tag: MessageTag.RPC,
-                    netid: client.me.control.netid,
-                    rpcid: RpcTag.SendChat,
-                    message: "poo baby"
-                }
-            ], true, client.host);
-        }, 2000);
-    });
+    await client.connect("NA", "weakeyes", parseInt(process.argv[2]));
 
     await client.joinGame(process.argv[3]);
     console.log("Joined game.");
+
+    client.on("player.snapto", ev => {
+        console.log(ev.data.player?.data?.name, ev.data.position);
+    });
+
+    client.on("player.move", ev => {
+        console.log(ev.data.player?.data?.name, ev.data.position);
+    });
 })();
