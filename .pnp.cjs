c50d383f58443cb2a03019ed87267edf4c9bb12f
Edward Smale <essmale2005@gmail.com>
2021-06-28 12:50:14 +0100

    Remove versions

--- .pnp.cjs
+++ .pnp.cjs
@@ -670,7 +670,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./packages/reactor/",
           "packageDependencies": [
             ["@skeldjs/reactor", "workspace:packages/reactor"],
-            ["@skeldjs/client", "workspace:packages/client"],
             ["@skeldjs/constant", "workspace:packages/constant"],
             ["@skeldjs/protocol", "workspace:packages/protocol"],
             ["@skeldjs/util", "workspace:packages/util"],
