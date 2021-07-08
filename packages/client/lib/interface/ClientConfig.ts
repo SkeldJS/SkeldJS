0e2e485389b448cf9d2bbc490ba634fe3c63b283
Edward Smale <essmale2005@gmail.com>
2021-07-08 12:31:29 +0100

    Add language option for client

--- packages/client/lib/interface/ClientConfig.ts
+++ packages/client/lib/interface/ClientConfig.ts
@@ -1,8 +1,13 @@
-import { HostableOptions } from "@skeldjs/core";
+import { GameKeyword, HostableOptions } from "@skeldjs/core";
 
 export interface ClientConfig extends HostableOptions {
     /**
      * Whether or not to allow host actions to take place.
      */
     allowHost?: boolean;
+
+    /**
+     * The client's language. Used to localise messages from the server.
+     */
+    language: GameKeyword;
 }
