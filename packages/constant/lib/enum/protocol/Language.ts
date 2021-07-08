7e08d401b54234ea69abc053aed3fae0f7c80213
Edward Smale <essmale2005@gmail.com>
2021-07-08 17:19:14 +0100

    Add language enum

---
+++ packages/constant/lib/enum/protocol/Language.ts
@@ -0,0 +1,18 @@
+export enum Language {
+    English,
+    SpanishAmericas,
+    PortugueseBrazil,
+    Portuguese,
+    Korean,
+    Russian,
+    Dutch,
+    Filipino,
+    French,
+    German,
+    Italian,
+    Japanese,
+    Spanish,
+    ChineseTraditional,
+    ChineseSimplified,
+    Irish
+}
--- packages/constant/lib/enum/protocol/index.ts
+++ packages/constant/lib/enum/protocol/index.ts
@@ -2,6 +2,7 @@ export * from "./ChatNoteType";
 export * from "./DisconnectReason";
 export * from "./GameDataMessageTag";
 export * from "./GetGameListTag";
+export * from "./Language";
 export * from "./RootMessageTag";
 export * from "./RpcMessageTag";
 export * from "./SendOption";
