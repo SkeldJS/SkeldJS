bc8d977c40330dd6974eb2c069a06b729e111ecf
Edward Smale <essmale2005@gmail.com>
2021-07-08 15:00:04 +0100

    Add 'attemptAuth' option

--- packages/client/lib/interface/ClientConfig.ts
+++ packages/client/lib/interface/ClientConfig.ts
@@ -7,6 +7,11 @@ export interface ClientConfig extends HostableOptions {
     allowHost: boolean;
 
     /**
+     * Whether to attempt to authorise with the server to connect to.
+     */
+    attemptAuth: boolean;
+
+    /**
      * The client's language. Used to localise messages from the server.
      */
     language: GameKeyword;
