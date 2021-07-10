ffccb4e98eeb884b9ac27298db86a2852641c81c
Edward Smale <essmale2005@gmail.com>
2021-07-10 21:11:53 +0100

    2.11.11 Remove @skeldjs/constant VoteState

--- packages/constant/lib/bitfields/VoteState.ts
+++
@@ -1,6 +0,0 @@
-export const VoteState = {
-    VotedFor: 0xf,
-    DidReport: 0x20,
-    DidVote: 0x40,
-    IsDead: 0x80,
-} as const;
--- packages/constant/lib/bitfields/index.ts
+++ packages/constant/lib/bitfields/index.ts
@@ -1,2 +1,1 @@
 export * from "./PlayerDataFlags";
-export * from "./VoteState";
