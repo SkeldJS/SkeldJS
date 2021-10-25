547f9b89415259017eaa31ee5d61f0319c9d667e
Edward Smale <essmale2005@gmail.com>
2021-10-25 22:29:22 +0100

    Add docs & examples

--- packages/translations/README.md
+++ packages/translations/README.md
@@ -1,5 +1,58 @@
-## @skeldjs/tasks
+## @skeldjs/translations
 
-This package contains utilities for managing tasks in Among Us, and while you can install it on its own with `npm install --save @skeldjs/tasks` or `yarn add @skeldjs/tasks`, it is one package of a bigger project, [skeldjs](https://github.com/skeldjs/SkeldJS).
+Utlities for translation keys in Among Us, such as synthesising quick chat messages into a single string. Meant to be installed separately with `npm install --save @skeldjs/translations` or `yarn add @skeldjs/translations`, and is one package of a bigger project, [skeldjs](https://github.com/skeldjs/SkeldJS).
 
-You can view auto-updating documentation for this package hosted at github pages at https://skeld.js.org/modules/tasks.html
+You can view auto-updating documentation for this package hosted at github pages at https://skeld.js.org/modules/translations.html
+
+## Basic Usage
+
+```ts
+client.on("meeting.votingcomplete", ev => {
+    if (ev.ejected) {
+        console.log(translation.formatString(StringNames.ExileTextNonConfirm, [ ev.ejected ], Language.English));
+    } else {
+        if (ev.tie) {
+            console.log(translation.getTranslation(StringNames.NoExileTie, Language.English));
+        } else {
+            console.log(translation.getTranslation(StringNames.NoExileSkip, Language.English));
+        }
+    }
+});
+```
+
+## Advanced Usage
+
+```ts
+import { Int2Code } from "@skeldjs/util";
+import { TranslationController } from "@skeldjs/translations";
+
+import * as skeldjs from "@skeldjs/client";
+import { Language } from "@skeldjs/client";
+
+(async () => {
+    const client = new skeldjs.SkeldjsClient("2021.6.30s");
+    const translation = new TranslationController(client);
+
+    console.log("Connecting to server..");
+    await client.connect("EU", "weakeyes");
+
+    console.log("Creating game..");
+    const code = await client.createGame(
+        {
+            maxPlayers: 10,
+            map: skeldjs.GameMap.MiraHQ,
+            numImpostors: 2
+        }
+    );
+
+    client.on("player.quickchat", ev => {
+        console.log(translation.serializeQuickChat(ev.chatMessage, Language.English));
+    });
+
+    console.log(
+        "Created game @ " +
+            Int2Code(code as number) +
+            " on EU servers"
+    );
+})();
+```
--- packages/translations/src/TranslationController.ts
+++ packages/translations/src/TranslationController.ts
@@ -1,15 +1,21 @@
-import { Hostable, Language, StringNames } from "@skeldjs/core";
+import { Hostable, Language, PlayerData, StringNames } from "@skeldjs/core";
 import { QuickChatMessageData, QuickChatPhraseMessageData, QuickChatPlayerMessageData, QuickChatSentenceMessageData } from "@skeldjs/protocol";
 
 import { AllTranslations } from "./translations";
 
+/**
+ * Represents a controller for a room to handle translations.
+ */
 export class TranslationController {
-    constructor(public readonly room: Hostable) {}
+    /**
+     * Create a translation controller, optionally passing in a room to connect
+     * it with player names for quick chat messages.
+     * @param room The room to connect the translation controller to.
+     */
+    constructor(public readonly room?: Hostable) {}
 
     private getPlayerName(playerId: number) {
-        const player = this.room.getPlayerByPlayerId(playerId);
-
-        return player?.info?.name || "";
+        return this.room?.getPlayerByPlayerId(playerId)?.info?.name || "";
     }
 
     private getQuickchatTranslation(stringName: StringNames, language: Language) {
@@ -22,8 +28,21 @@ export class TranslationController {
         return translation;
     }
 
-    private formatString(str: string, elements: (number|QuickChatPlayerMessageData)[], language: Language) {
-        return str.replace(/\{\d\}/g, x => {
+    /**
+     * Format a translation string containing {X} templating. Also substitutes
+     * player names if the translation controller is connected to a room in the
+     * constructor.
+     * @param str The string to format against.
+     * @param elements The elements to format the string with.
+     * @param language The language of the string.
+     * @returns The formatted string.
+     */
+    formatString(str: string|StringNames, elements: (number|QuickChatPlayerMessageData|PlayerData)[], language: Language) {
+        const translationString = typeof str === "string"
+            ? str
+            : this.getTranslation(str, language);
+
+        return translationString.replace(/\{\d\}/g, x => {
             const elementI = parseInt(x[1]);
             const element = elements[elementI];
 
@@ -35,10 +54,21 @@ export class TranslationController {
                 return this.getTranslation(element as StringNames, language);
             }
 
+            if (element instanceof PlayerData) {
+                return element.info?.name || "";
+            }
+
             return this.getPlayerName(element.playerId);
         });
     }
 
+    /**
+     * Get the single translation of a string.
+     * @param stringName The string name to get.
+     * @param language The language to translate the string name to.
+     * @returns The translated string, or an empty string if an invalid string
+     * name was passed.
+     */
     getTranslation(stringName: StringNames, language: Language): string {
         const translation = AllTranslations[language][stringName] || AllTranslations[Language.English][stringName];
 
@@ -50,9 +80,16 @@ export class TranslationController {
             return translation;
         }
 
-        return translation[Object.keys(translation)[0]]; // if it's an object then just get the first one, doesn't really matter.
+        return translation["default"] || translation[Object.keys(translation)[0]]; // if it's an object then just get the first one, doesn't really matter.
     }
 
+    /**
+     * Synthesise a quick chat message into a single string, considering translations
+     * and player names.
+     * @param quickChatMessage The message to synthesise.
+     * @param language The language to synthesise the message into.
+     * @returns The synthesised message.
+     */
     serializeQuickChat(quickChatMessage: QuickChatMessageData, language: Language): string {
         if (quickChatMessage instanceof QuickChatPlayerMessageData) {
             return this.getPlayerName(quickChatMessage.playerId);
@@ -62,7 +99,7 @@ export class TranslationController {
             const formatTranslation = this.getQuickchatTranslation(quickChatMessage.formatString, language);
 
             if (typeof formatTranslation === "string") {
-                return this.formatString(this.getTranslation(quickChatMessage.formatString, language), quickChatMessage.elements, language);
+                return this.formatString(quickChatMessage.formatString, quickChatMessage.elements, language);
             }
 
             const elementTypes = [];
@@ -78,7 +115,9 @@ export class TranslationController {
 
             const keyByElements = elementTypes.join("_");
 
-            const translationByElements = formatTranslation[keyByElements] || formatTranslation[Object.keys(formatTranslation)[0]];
+            const translationByElements = formatTranslation[keyByElements]
+                ?? formatTranslation["default"]
+                ?? formatTranslation[Object.keys(formatTranslation)[0]];
 
             if (!translationByElements)
                 return "";
