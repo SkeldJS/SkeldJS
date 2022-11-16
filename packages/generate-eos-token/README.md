1d066306a15a572db6c458e7e8f0b359cbf0005c
Edward Smale <essmale2005@gmail.com>
2022-11-16 22:51:32 +0000

    Add generate-eos-token package

---
+++ packages/generate-eos-token/README.md
@@ -0,0 +1,5 @@
+## @skeldjs/events
+
+A simple cancellable asynchronous event emitter.
+
+You can view auto-updating documentation for this package hosted at github pages at https://skeld.js.org/modules/data.html
---
+++ packages/generate-eos-token/index.ts
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+import { EosHttpApi } from "@skeldjs/eos";
+
+const parsedArgs: Record<string, string> = {};
+const argv = process.argv.slice(2);
+let currentArg: string|undefined;
+while ((currentArg = argv.shift()) !== undefined) {
+    if (currentArg.startsWith("--")) {
+        const optionName = currentArg.substring(2);
+        const optionValue = argv.shift();
+
+        if (!optionValue)
+            continue;
+
+        parsedArgs[optionName] = optionValue;
+    }
+}
+
+const clientId = parsedArgs.clientId || "xyza7891qtrmoYLr86we6DlfCA1RRsp8";
+const clientSecret = parsedArgs.clientSecret || "nGThQanzvthA2HPaARXe/xutzsKyx5WJveNkBx44ti4";
+const deploymentId = parsedArgs.deploymentId || "3ce14d8292084c80a8364a8b5f0dfbf4";
+const displayName = parsedArgs.displayName || Math.random().toString(16).substr(2);
+
+(async () => {
+    const randomDeviceModel = EosHttpApi.generateRandomDeviceModel();
+    const { access_token } = await EosHttpApi.authRequestGetDeviceIdAccessToken(clientId, clientSecret, randomDeviceModel);
+
+    const account = await EosHttpApi.authRequestEosAccessToken({
+        grantType: "external_auth",
+        externalAuthType: "deviceid_access_token",
+        clientId: clientId,
+        clientSecret: clientSecret,
+        deploymentId: deploymentId,
+        nonce: "aaaaaaaaab",
+        displayName: displayName,
+        externalAuthToken: access_token,
+    });
+
+    console.log("====================================================");
+    console.log("Access token: %s", access_token);
+    console.log("====================================================");
+    console.log("ID token: %s", account.id_token);
+    console.log("====================================================");
+    console.log("EOS product id: %s", account.product_user_id);
+    console.log("====================================================");
+    console.log("Expires at: %s", account.expires_at);
+})();
